75f3ed6d2546f7f1c070c51a678e988d2755f85f
Andrew Shackelford <Andrew@Andrews-MacBook-Air.local>
2015-05-27 01:01:25 -0400

    Initial Commit

---
+++ RTHWatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RTHWatch
+//
+//  Created by Andrew Shackelford on 5/27/15.
+//  Copyright (c) 2015 Golden Dog Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RTHWatch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RTHWatch
+//
+//  Created by Andrew Shackelford on 5/27/15.
+//  Copyright (c) 2015 Golden Dog Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
